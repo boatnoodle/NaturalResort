ee3b55349ddcfcb0f6c3a3a625e486fd7231c81d
boatnoodle <nattasit31@hotmail.com>
2018-05-22 23:31:54 +0700

    update agent code

--- config/stratagies/local.js
+++ config/stratagies/local.js
@@ -1,6 +1,6 @@
-var passport = require("passport");
-var LocalStrategy = require("passport-local").Strategy;
-var con = require("../../config/mysql");
+var passport = require("passport")
+var LocalStrategy = require("passport-local").Strategy
+var con = require("../../config/mysql")
 
 module.exports = function() {
     passport.use("local-signin", new LocalStrategy({
@@ -12,12 +12,12 @@ module.exports = function() {
             con.query(`SELECT * FROM user WHERE username = '${username}' AND password = '${password}'`,
                 function(err, rows) {
                     if (err) {
-                        return done(err);
+                        return done(err)
                     }
                     if (!rows.length) {
-                        return done(null,false, req.flash("error", "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง"));
+                        return done(null,false, req.flash("error", "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง"))
                     }
-                    return done(null, rows[0]);
+                    return done(null, rows[0])
                 }
             )}
         )
@@ -33,15 +33,15 @@ module.exports = function() {
                     return done(err)
                 }
                 if(rows.length > 0){
-                    return done(null,false,req.flash('error', "ชื่อผู้ใช้นี้ถูกใช้งานแล้ว"));
+                    return done(null,false,req.flash('error', "ชื่อผู้ใช้นี้ถูกใช้งานแล้ว"))
                 }else{
                     var newUser = {}
-                    newUser.username = username;
-                    newUser.password = password;
-                    newUser.name = req.body.name;
+                    newUser.username = username
+                    newUser.password = password
+                    newUser.name = req.body.name
                     con.query(`INSERT INTO user (username,password,name) VALUES ('${newUser.username}','${newUser.password}','${newUser.name}')`,function(err,rows){
                         newUser.id = rows.insertId
-                        return done(null, false, req.flash("success", "เพิ่มผู้ใช้สำเร็จ"));
+                        return done(null, false, req.flash("success", "เพิ่มผู้ใช้สำเร็จ"))
                     })
                 }
             })
@@ -53,28 +53,56 @@ module.exports = function() {
         passReqToCallback: true
     },
     function(req, username, password, done) {
-            con.query(`SELECT * FROM user WHERE username = '${username}'`,function(err,rows){
-                if(err){
+        var userId = req.body.userId,
+        name = req.body.name
+        con.query(`UPDATE user SET name = '${name}', password = '${password}' WHERE userId = ${userId}`,
+            function(err, rows) {
+                if (err) {
                     return done(err)
-                }
-                if(rows.length > 0){
-                    return done(null,false,req.flash('error', "ชื่อผู้ใช้นี้ถูกใช้งานแล้ว"));
                 }else{
-                    var userId = req.body.userId,
-                        name = req.body.name;
-                    con.query(`UPDATE user SET username = '${username}', password = '${password}', name = '${name}' WHERE userId = ${userId}`,
-                        function(err, rows) {
-                            if (err) {
-                                return done(err);
-                            }else{
-                                return done(null,false, req.flash("success", "แก้ไขข้อมูลผู้ใช้สำเร็จ"));
-                            }
+                    con.query(`SELECT * FROM user WHERE username = '${username}'`,function(err,rows){
+                        if(err){
+                            return done(err)
                         }
-                    )
+                        if(rows.length > 0){
+                            return done(null,false,req.flash('error', "ชื่อผู้ใช้นี้ถูกใช้งานแล้ว"))
+                        }else{
+                            con.query(`UPDATE user SET username = '${username}' WHERE userId = ${userId}`,
+                                function(err, rows) {
+                                    if (err) {
+                                        return done(err)
+                                    }else{
+                                        return done(null,false, req.flash("success", "แก้ไขข้อมูลผู้ใช้สำเร็จ"))
+                                    }
+                                }
+                            )
+                        }
+                    })
                 }
-            })
+            }
+        )
+            // con.query(`SELECT * FROM user WHERE username = '${username}'`,function(err,rows){
+            //     if(err){
+            //         return done(err)
+            //     }
+            //     if(rows.length > 0){
+            //         return done(null,false,req.flash('error', "ชื่อผู้ใช้นี้ถูกใช้งานแล้ว"))
+            //     }else{
+            //         var userId = req.body.userId,
+            //             name = req.body.name
+            //         con.query(`UPDATE user SET username = '${username}', password = '${password}', name = '${name}' WHERE userId = ${userId}`,
+            //             function(err, rows) {
+            //                 if (err) {
+            //                     return done(err)
+            //                 }else{
+            //                     return done(null,false, req.flash("success", "แก้ไขข้อมูลผู้ใช้สำเร็จ"))
+            //                 }
+            //             }
+            //         )
+            //     }
+            // })
         }
     )
 )
 
-};
+}
--- controller/agentController.js
+++ controller/agentController.js
@@ -1,58 +1,80 @@
-var db = require("../config/mysql");
+var db = require("../config/mysql")
 module.exports = {
   getAgent: function(req, res, next) {
-    var sql = `SELECT * FROM agent WHERE 1`;
+    var sql = `SELECT * FROM agent WHERE 1`
     db.query(sql, function(error, result) {
       if (error) {
-        throw error;
+        throw error
       } else {
-        res.render("setting/agent", { data: result, status: req.flash() });
+        var data = {}
+        var arr = []
+        result.forEach(element => {
+          if(element.statusWalkIn == 0){
+            var statusName = "No walk in"
+          }else{
+            var statusName = "Walk in"
+          }
+          data = {
+            agentId: element.agentId,
+            agentName: element.agentName,
+            statusWalkIn: element.statusWalkIn,
+            statusName: statusName
+          }
+          arr.push(data)
+        })
+        res.render("setting/agent", { data: arr, status: req.flash() })
       }
-    });
+    })
   },
   getAgentById: function(req, res, next){
-    var agentId = req.params.id;
+    var agentId = req.params.id
     db.query(`SELECT * FROM agent WHERE agentId = ${agentId}`,function(error,rows){
         if(error){
-          throw error;
+          throw error
         }else{
-          res.json(rows[0]);
+          res.json(rows[0])
         }
     })
   },
   addAgent: function(req, res, next) {
     var agentName = req.body.agentName,
-        sql = `INSERT INTO agent (agentName) VALUES ('${agentName}')`;
+        statusWalkIn = req.body.statusWalkIn
+        if(statusWalkIn == undefined){
+          statusWalkIn = 0
+        }
+        sql = `INSERT INTO agent (agentName,statusWalkIn) VALUES ('${agentName}',${statusWalkIn})`
     db.query(sql, function(error, result) {
       if (error) {
-        req.flash("error", "เพิ่มข้อมูลไม่สำเร็จ");
+        req.flash("error", "เพิ่มข้อมูลไม่สำเร็จ")
       } else {
-        req.flash("success", "เพิ่มข้อมูลสำเร็จ");
+        req.flash("success", "เพิ่มข้อมูลสำเร็จ")
       }
-      res.redirect("/setting/agent");
-    });
+      res.redirect("/setting/agent")
+    })
   },
   updateAgent: function(req, res, next){
     var agentId = req.body.agentId,
-        agentName = req.body.agentName;
-    db.query(`UPDATE agent SET agentName = '${ agentName }' WHERE agentId = ${ agentId }`, function(error, rows){
+        agentName = req.body.agentName,
+        statusWalkIn = req.body.statusWalkIn
+        console.log(statusWalkIn)
+    db.query(`UPDATE agent SET agentName = '${ agentName }', statusWalkIn = ${statusWalkIn} WHERE agentId = ${ agentId }`, function(error, rows){
       if (error){
-        req.flash("error", "ไม่สามารถแก้ไขข้อมูลตัวแทนได้");
+        req.flash("error", "ไม่สามารถแก้ไขข้อมูลตัวแทนได้")
       } else{
-        req.flash("success", "แก้ไขข้อมูลตัวแทนสำเร็จ");
+        req.flash("success", "แก้ไขข้อมูลตัวแทนสำเร็จ")
       }
-      res.redirect("/setting/agent");
+      res.redirect("/setting/agent")
     })
   },
   deleteAgent: function(req, res, next){
-    var agentId = req.params.id;
+    var agentId = req.params.id
     db.query(`DELETE FROM agent WHERE agentId = ${agentId}`,function(error, rows){
       if(error){
-        req.flash('error', 'ไม่สามารถลบตัวแทนได้');
+        req.flash('error', 'ไม่สามารถลบตัวแทนได้')
       }else{
-        req.flash('success', 'ลบตัวแทนสำเร็จ');
+        req.flash('success', 'ลบตัวแทนสำเร็จ')
       }
-      res.redirect("/setting/agent");
+      res.redirect("/setting/agent")
     })
   }
-};
+}
